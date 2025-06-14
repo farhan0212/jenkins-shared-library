53b7335405e50bdbbcaefcd368e30fddd66e86c4
farhan0212 <farhan.ramadan0212@gmail.com>
2025-06-14 21:27:48 +0700

    feat: add docker build and push functions for image management

---
+++ vars/docker.groovy
@@ -0,0 +1,15 @@
+def docker_build(imageName, buildNumber){
+    echo("image name = ${imageName}")
+    echo("build number = ${buildNumber}")
+    sh("docker build -t ${imageName}:${buildNumber} .")
+}
+
+def dockerhub_push(credentialsId, imageName, buildNumber){
+    withCredentials([usernamePassword(
+            credentialsId: credentialsId,
+            usernmameVariable: 'DOCKER_USER',
+            passwordVariable: 'DOCKER_PASS'
+        )])
+        sh("echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin")
+        sh("docker push ${imageName}:${buildNumber}")
+}
