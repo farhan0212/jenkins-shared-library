f94cb000e86ffb2765b9d21f6a9f5bd22c06c616
farhan0212 <farhan.ramadan0212@gmail.com>
2025-06-14 16:13:57 +0700

    add npm_install function to handle node module installation

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,3 +1,9 @@
 def world() {
     echo("Hello, World!")
+} 
+
+def npm_install() {
+    echo(message: 'Running installation node modules')
+    sh('npm install')
+    echo(message: 'post installation node modules')
 }
