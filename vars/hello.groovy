6a9b09fc4dadd90595b687a6508b987c288aecb9
farhan0212 <farhan.ramadan0212@gmail.com>
2025-06-14 19:35:51 +0700

    refactor npm_install function to npm_do for improved command handling

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -2,9 +2,8 @@ def world() {
     echo("Hello, World!")
 } 
 
-def npm_install() {
-    echo(message: 'Running installation node modules')
-    sh('npm install')
-    sh('npm run build')
-    echo(message: 'post installation node modules')
+def npm_do(List commands) {
+    for (command in commands) {
+        sh("npm run ${command}")
+    }
 }
