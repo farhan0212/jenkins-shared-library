73a30d5585ee33e11f290d8f93dffed018eea20d
farhan0212 <farhan.ramadan0212@gmail.com>
2025-06-15 11:40:34 +0700

    fix: add ssh_action function for remote command execution

--- vars/dockerHelper.groovy
+++ vars/dockerHelper.groovy
@@ -14,3 +14,15 @@ def docker_build_push(dockerCredentialsId, imageName, buildNumber){
 def hello(){
     sh(" echo hello world")
 }
+
+def ssh_action(sshCredentials, SSH_USERNAME, SSH_HOST) {
+    sshagent(credentials: [sshCredentials]){
+        sh'''
+            ssh -o StrictHostKeyChecking=no ${SSH_USERNAME}@${SSH_HOST} '
+            echo "Running commands on remote server"
+            ls -la
+            whoami
+            '        
+            '''
+    }
+}
